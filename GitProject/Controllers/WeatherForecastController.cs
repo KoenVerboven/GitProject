f0f11a233417d0d0c56ebbc3b0a3bda2a523a413
Verboven Koen <koenverboven@gmail.com>
2025-03-18 11:52:35 +0100

    Rename Summaries

--- GitProject/Controllers/WeatherForecastController.cs
+++ GitProject/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace GitProject.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly string[] WheaterSummaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Very Cold", "Very Hot"
         };
@@ -27,7 +27,7 @@ namespace GitProject.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = WheaterSummaries[Random.Shared.Next(WheaterSummaries.Length)]
             })
             .ToArray();
         }
