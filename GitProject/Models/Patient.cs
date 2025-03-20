071de1bdd5e030424b45bee0d482172d2adfe5a9
Verboven Koen <koenverboven@gmail.com>
2025-03-20 18:30:51 +0100

    Adding class Patient

---
+++ GitProject/Models/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace GitProject.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public DateOnly dateOfBirth { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
