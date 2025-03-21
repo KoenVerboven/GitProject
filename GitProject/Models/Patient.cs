25bc4a9e354b2951cd3629fb3203bf6dfefafddc
Verboven Koen <koenverboven@gmail.com>
2025-03-21 15:36:44 +0100

    Add FullName

--- GitProject/Models/Patient.cs
+++ GitProject/Models/Patient.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Lastname { get; set; }
         public string Firstname { get; set; }
+        public string FullName { get; set; }
         public DateOnly dateOfBirth { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime UpdateDate { get; set; }
