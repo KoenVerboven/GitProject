e0079afd07a633f1034ba8de784fe21e7f7f372b
Verboven Koen <koenverboven@gmail.com>
2025-10-25 17:03:02 +0200

    Add models HospitalGroup and HospitalDepartment

---
+++ GitProject/Models/HospialGroup.cs
@@ -0,0 +1,8 @@
+﻿namespace GitProject.Models
+{
+    public class HospialGroup
+    {
+        public  int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ GitProject/Models/HospitalDepartment.cs
@@ -0,0 +1,8 @@
+﻿namespace GitProject.Models
+{
+    public class HospitalDepartment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
