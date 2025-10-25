0b200b62880eb4c2b5e17d02370039472c265eaa
Verboven Koen <koenverboven@gmail.com>
2025-10-25 18:06:26 +0200

    Add class MedicalWorker

---
+++ GitProject/Models/MedicalWorker.cs
@@ -0,0 +1,13 @@
+﻿namespace GitProject.Models
+{
+    public class MedicalWorker
+    {
+        public int Id { get; set; }
+        public required string LastName { get; set; }
+        public required string FirstName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public DateOnly DateOfBirth { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
